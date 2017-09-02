7b76c22222f0500d7a01b12479c6c3f45a5a3b54
Colin Kiama <colinkiama@hotmail.co.uk>
2017-09-02 16:10:12 +0100

    Add project files.

---
+++ appLauncher/Control/appControl.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace appLauncher.Control
+{
+    public sealed partial class appControl : UserControl
+    {
+        public AppListEntry appItem { get { return this.DataContext as AppListEntry; } }
+        public appControl()
+        {
+            this.InitializeComponent();
+            this.DataContextChanged += (s, e) => Bindings.Update();
+        }
+
+        private async void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (appItem != null)
+            {
+                var logoStream = appItem.DisplayInfo.GetLogo(new Size(50,50));
+                BitmapImage imageFromStream = new BitmapImage();
+                IRandomAccessStreamWithContentType whatIWant = await logoStream.OpenReadAsync();
+                imageFromStream.SetSource(whatIWant);
+                this.appIcon.Source = imageFromStream;
+            }
+        }
+    }
+}
---
+++ appLauncher/Helpers/packageHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Core;
+using Windows.Management.Deployment;
+
+namespace appLauncher.Helpers
+{
+   public class packageHelper
+   {
+       public static PackageManager pkgManager = new PackageManager();
+        public static ObservableCollection<AppListEntry> getAllApps()
+        {
+
+            var listOfInstalledPackages = pkgManager.FindPackagesForUserWithPackageTypes("", PackageTypes.Bundle);
+            List<Package> packages = new List<Package>();
+            packages = listOfInstalledPackages.ToList();
+            ObservableCollection<AppListEntry> appListEntries = new ObservableCollection<AppListEntry>();
+            int count = packages.Count();
+            for (int i = 0; i < count; i++)
+            {
+                List<AppListEntry> singleAppListEntries = new List<AppListEntry>();
+                Task<IReadOnlyList<AppListEntry>> getAppEntriesTask = packages[i].GetAppListEntriesAsync().AsTask();
+                getAppEntriesTask.Wait();
+                singleAppListEntries = getAppEntriesTask.Result.ToList();
+                appListEntries.Add(singleAppListEntries[0]);
+                
+            }
+            return appListEntries;
+        }
+   }
+}
