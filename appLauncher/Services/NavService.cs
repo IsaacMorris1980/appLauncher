f4a7c06f67711a66cf3fa65962e18cf4c4a18e0b
Colin Kiama <colinkiama@gmail.com>
2019-05-27 00:07:34 +0100

    Added navigation service

---
+++ appLauncher/Services/NavService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace appLauncher.Services
+{
+    public class NavService
+    {
+        private static Frame _frame = null;
+        public static NavService Instance = new NavService();
+        public static void CreateInstance(Frame frame)
+        {
+            _frame = frame;
+
+        }
+
+        public void GoBack()
+        {
+            if (_frame.CanGoBack)
+            {
+                _frame.GoBack();
+            }
+        }
+
+        public void GoForward()
+        {
+            if (_frame.CanGoForward)
+            {
+                _frame.GoForward();
+            }
+        }
+
+
+        public bool Navigate(Type sourcePageType)
+        {
+            return _frame.Navigate(sourcePageType);
+        }
+
+        public bool Navigate(Type sourcePageType, object parameter)
+        {
+            return _frame.Navigate(sourcePageType, parameter);
+        }
+
+        public bool Navigate(Type sourcePageType, object parameter, NavigationTransitionInfo infoOverride)
+        {
+            return _frame.Navigate(sourcePageType, parameter, infoOverride);
+        }
+
+        public bool IsCurrentPageOfType(Type typeQuery)
+        {
+            return _frame.SourcePageType.Equals(typeQuery);
+        }
+    }
+}
