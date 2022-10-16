78c248ee573214ea1493400cae45c9e6e21ae88a
Isaac Morris <morris.isaac@gmail.com>
2022-10-15 21:28:37 -0500

    Optimised (#84)
    * starting change
    
    * updating
    
    * new changes
    
    * a  few more changes
    still need to add a few features
    
    * updating needing to add anaylitics and crash tracking plus  begining of custom settings
    
    * continueing to work on multiple tickets
    
    * almost done need to update settings page and add crash reporting to more of the app along with analytics
    
    * redesigning main page
    
    * Almost complete a few more tweaks and new release will be created
    
    * saving new changes
    
    * Need to add more crash reporting and analytics plus need to redesign the settings page
    
    * Latest updates before releasing new build

---
+++ appLauncher/Core/Model/PageBackgrounds.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace appLauncher.Core.Model
+{
+    public class PageBackgrounds : ModelBase
+    {
+        public string pageBackgroundImageFullPathLocation { get; set; }
+        public string pageBackgroundImageDisplayName { get; set; }
+        [JsonIgnore]
+        public string pageBackgroundImageBytes { get; set; }
+        public BitmapImage pageBackgroundDisplayImage { get; set; }
+
+
+
+    }
+}
