b4eef2b26ee4627123e01628d811a95a47afc0a0
Isaac Morris <morris.isaac@gmail.com>
2022-10-15 22:45:40 -0500

    Revert "Optimised (#84)"
    This reverts commit 78c248ee573214ea1493400cae45c9e6e21ae88a.

---
+++ appLauncher/Core/DesktopBackButton.cs
@@ -0,0 +1,20 @@
+﻿using Windows.UI.Core;
+
+namespace appLauncher.Core
+{
+    public static class DesktopBackButton
+    {
+        public static void ShowBackButton()
+        {
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+
+        }
+
+        public static void HideBackButton()
+        {
+
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+        }
+
+    }
+}
