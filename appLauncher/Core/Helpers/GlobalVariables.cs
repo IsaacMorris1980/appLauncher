197af8f0ce7af14400894a29165a0c12b7b7d416
Isaac Morris <morris.isaac@gmail.com>
2023-02-03 21:15:03 -0600

    begining rewritting global varibles

--- appLauncher/Core/Helpers/GlobalVariables.cs
+++ appLauncher/Core/Helpers/GlobalVariables.cs
@@ -11,12 +11,9 @@ namespace appLauncher.Core.Helpers
         public static int appsperscreen { get; private set; }
         public static DraggedItem itemdragged { get; set; } = new DraggedItem();
         public static int columns { get; set; }
-        public static int oldindex { get; set; }
-        public static int newindex { get; set; }
         public static int pagenum { get; private set; }
         public static int NumofPages { get; private set; }
         public static bool isdragging { get; set; }
-        public static bool bgimagesavailable { get; set; }
 
         private static StorageFolder localFolder = ApplicationData.Current.LocalFolder;
         public static Point startingpoint { get; set; }
