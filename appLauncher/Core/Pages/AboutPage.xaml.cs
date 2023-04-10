3e7902ed45fb5fc83cdfc0658a25b329c5611b22
Isaac Morris <morris.isaac@gmail.com>
2023-04-09 19:02:56 -0500

    About page created

---
+++ appLauncher/Core/Pages/AboutPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace appLauncher.Core.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AboutPage : Page
+    {
+        private string appversion = $"App Version: {Helpers.SettingsHelper.totalAppSettings.AppVersion}";
+        public AboutPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Home_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+
+        private void SettingsPage_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(SettingsPage));
+        }
+
+    }
+}
