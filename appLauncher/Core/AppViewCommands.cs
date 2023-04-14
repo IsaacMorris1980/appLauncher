bf3c69f797bbea68802acaf05978c30c4ee02061
Isaac Morris <morris.isaac@gmail.com>
2023-04-14 08:45:10 -0500

    Fixed options menu

---
+++ appLauncher/Core/AppViewCommands.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml;
+
+namespace appLauncher.Core.ViewModels
+{
+    internal class AppViewCommands
+    {
+        public void dragEvent(object sender, DragEventArgs e)
+        {
+
+        }
+
+    }
+}
