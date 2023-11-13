0b850e5666d9d69f40960199a8aef7d4e8fef5ba
Isaac Morris <morris.isaac@gmail.com>
2023-11-12 22:09:38 -0600

    Add folder polished (#235)
    Fixed some app folder screen issues. Hot fix for app folder issue in
    last build

---
+++ appLauncher/Core/Pages/FolderNamePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml.Controls;
+
+// The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace appLauncher.Core.Pages
+{
+    public sealed partial class FolderNamePage : ContentDialog
+    {
+        public FolderNamePage()
+        {
+            this.InitializeComponent();
+
+        }
+        public string FolderName { get; set; }
+
+        private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            FolderName = SetFolderName.Text;
+        }
+
+        private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            this.Hide();
+        }
+    }
+}
